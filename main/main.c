4ab5d7ea37d857ba7f7c4a86b8bb628de41d768e
Helena-Mansour <helenasm@al.insper.edu.br>
2026-02-24 15:27:55 -0300

    LED pisca ao apertar o botão

--- main/main.c
+++ main/main.c
@@ -4,14 +4,38 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <stdio.h>
-#include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+const int BTN = 1;
+const int LED = 0;
+
 
 int main() {
-    stdio_init_all();
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+  stdio_init_all();
+
+  gpio_init(BTN);
+  gpio_set_dir(BTN, GPIO_IN);
+  gpio_pull_up(BTN);
+
+  
+  gpio_init(LED);
+  gpio_set_dir(LED, GPIO_OUT);
+
+
+
+  
+
+  while (true) {
+    if (!gpio_get(BTN)) {
+      gpio_put(LED, 1);
+      sleep_ms(300);
+      gpio_put(LED, 0);
+         
+      
     }
-}
+    
+     
+    }
+  }
