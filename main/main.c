80551f7c7b507a9fa0b3ef8bd2983fb5d409d2a6
Helena-Mansour <helenasm@al.insper.edu.br>
2026-03-24 14:47:56 -0300

    .

--- main/main.c
+++ main/main.c
@@ -1,123 +1,125 @@
 #include <stdio.h>
-#include <string.h> 
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
-#include "hardware/timer.h"
-#include "hardware/irq.h"
 
-const int BTN_PIN = 21;
-const int LED_PIN = 16;
+const int BTN_PIN_G = 20;
+const int BTN_PIN_Y = 19;
+const int BTN_PIN_B = 18;
 
-volatile bool pisca = false;
+const int LED_PIN_G = 15;
+const int LED_PIN_Y = 14;
+const int LED_PIN_B = 17;
 
-//botao precisonado
-volatile bool btn_press = false;
+//flag da hora que cada um deve piscar
+volatile bool ativo_g = false;
+volatile bool ativo_y = false;
+volatile bool ativo_b = false;
 
-//quando comecou a apertar
-volatile uint32_t t_inicial = 0;
+// CALLBACK DOS BOTÕES
+void btn_callback(uint gpio, uint32_t events){
 
-//quanto tempo segurou
-volatile uint32_t t_pressionado = 500;
-
-//botao solto
-volatile bool atualiza = false;
-
-//hora de parar
-volatile bool parar = false;
-
-//led rodando
-volatile bool ativo = false; 
-
-void btn_callback(uint gpio, uint32_t events) {
-    if(events == 0x4){ //botao apertado
-        btn_press = true;
-
-        //horario atual
-        t_inicial = to_ms_since_boot(get_absolute_time()); 
+    // Verde aperta liga e quando solta desliga
+    if(gpio == BTN_PIN_G){ 
+        if(events & GPIO_IRQ_EDGE_FALL){
+            ativo_g = true;
+        }
+        if(events & GPIO_IRQ_EDGE_RISE){
+            ativo_g = false;
+        }
     }
-    if(events == 0x8){ //botao solto
-        uint32_t t_final = to_ms_since_boot(get_absolute_time());
 
-        //horario final menos inicial
-        t_pressionado = t_final - t_inicial;
-        btn_press = false;
-        atualiza = true;
+    // amarelo aprtou inverte o estado
+    if(gpio == BTN_PIN_Y){
+        if(events & GPIO_IRQ_EDGE_FALL){
+            ativo_y = !ativo_y;
+        }
     }
-}
 
-bool timer_callback(repeating_timer_t *rt) {
-     //so pisca se nao tiver pressionado e se esta ativo
-    if(!btn_press && ativo){
-        pisca = true;
+    // azul apertou inverte o estado
+    if(gpio == BTN_PIN_B){
+        if(events & GPIO_IRQ_EDGE_RISE){
+            ativo_b = !ativo_b;
+        }
     }
-    return true;
-}
-
-int64_t alarm_callback(alarm_id_t id, void *user_data) {
-    parar = true;
-    return 0;
 }
 
 int main() {
     stdio_init_all();
 
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    gpio_put(LED_PIN, 0);
-    
-    gpio_init(BTN_PIN);
-    gpio_set_dir(BTN_PIN, GPIO_IN);
-    gpio_pull_up(BTN_PIN);
+    // BOTÕES
+    gpio_init(BTN_PIN_G);
+    gpio_set_dir(BTN_PIN_G, GPIO_IN);
+    gpio_pull_up(BTN_PIN_G);
+
+    gpio_init(BTN_PIN_Y);
+    gpio_set_dir(BTN_PIN_Y, GPIO_IN);
+    gpio_pull_up(BTN_PIN_Y);
+
+    gpio_init(BTN_PIN_B);
+    gpio_set_dir(BTN_PIN_B, GPIO_IN);
+    gpio_pull_up(BTN_PIN_B);
+
+    // IMPORTANTE: callback em TODOS os botões
     gpio_set_irq_enabled_with_callback(
-        BTN_PIN,
+        BTN_PIN_G,
         GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE,
         true,
         &btn_callback
     );
 
-    repeating_timer_t timer;
+    gpio_set_irq_enabled_with_callback(
+        BTN_PIN_Y,
+        GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE,
+        true,
+        &btn_callback
+    );
 
-    //timer pisca a cada 200ms
-    add_repeating_timer_ms(200, timer_callback, NULL, &timer);
+    gpio_set_irq_enabled_with_callback(
+        BTN_PIN_B,
+        GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE,
+        true,
+        &btn_callback
+    );
 
-    bool led_estado = false;
+    // LEDS
+    gpio_init(LED_PIN_G);
+    gpio_set_dir(LED_PIN_G, GPIO_OUT);
 
-    while (true) {
+    gpio_init(LED_PIN_Y);
+    gpio_set_dir(LED_PIN_Y, GPIO_OUT);
 
-        // enquanto pressiona o LED apagado
-        if(btn_press){ 
-            gpio_put(LED_PIN,0);
+    gpio_init(LED_PIN_B);
+    gpio_set_dir(LED_PIN_B, GPIO_OUT);
+
+    bool estado = false;
+
+    while(1){
+
+        // pisca global sincronizado
+        estado = !estado;
+
+        // VERDE
+        if(ativo_g){
+            gpio_put(LED_PIN_G, estado);
+        } else {
+            gpio_put(LED_PIN_G, 0);
         }
 
-        // piscar LED (só se ativo)
-        if(pisca && !btn_press && ativo){
-            led_estado = !led_estado;
-            gpio_put(LED_PIN, led_estado);
-            pisca = false;
+        // AMARELO
+        if(ativo_y){
+            gpio_put(LED_PIN_Y, estado);
+        } else {
+            gpio_put(LED_PIN_Y, 0);
         }
 
-        // quando solta botão reinicia inicia ciclo
-        if(atualiza){
-            //garante q o tempo minimo seja 50 ms
-            if(t_pressionado < 50){
-                t_pressionado = 50;
-            }
-
-            ativo = true; // ativa o sistema
-
-            //led pisca por t_pressionado de tempo
-            add_alarm_in_ms(t_pressionado, alarm_callback, NULL, false);
-
-            atualiza = false;
+        // AZUL
+        if(ativo_b){
+            gpio_put(LED_PIN_B, estado);
+        } else {
+            gpio_put(LED_PIN_B, 0);
         }
 
-        // parar quando tempo acabar
-        if(parar){
-            parar = false;
-            ativo = false;
-            gpio_put(LED_PIN, 0);
-        }
+        sleep_ms(200);
     }
-
-    return 0;
 }
+
