a0632bebdd7a872ce7e0c980012ed1a8804086b9
Helena-Mansour <helenasm@al.insper.edu.br>
2026-03-24 14:45:49 -0300

    .

--- main/main.c
+++ main/main.c
@@ -1,147 +1,122 @@
-
-
 #include <stdio.h>
-
-#include "hardware/adc.h"
+#include <string.h> 
+#include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/timer.h"
-#include "pico/stdlib.h"
+#include "hardware/irq.h"
 
-const int BTN_PIN_G = 20;
-const int BTN_PIN_Y = 19;
+const int BTN_PIN = 21;
+const int LED_PIN = 16;
 
-const int LED_PIN_G = 15;
-const int LED_PIN_Y = 14;
+volatile bool pisca = false;
 
-//botao verde precionado
-volatile bool btn_g_press = false;
+//botao precisonado
+volatile bool btn_press = false;
 
-//botao amarelo precionado
-volatile bool btn_y_press = false;
+//quando comecou a apertar
+volatile uint32_t t_inicial = 0;
 
-//hora do botao piscar
-volatile bool pisca_y = false;
-volatile bool pisca_g = false;
+//quanto tempo segurou
+volatile uint32_t t_pressionado = 500;
 
-//tempo acabou
-volatile bool alarme_g = false;
-volatile bool alarme_y = false;
+//botao solto
+volatile bool atualiza = false;
 
+//hora de parar
+volatile bool parar = false;
+
+//led rodando
+volatile bool ativo = false; 
 
 void btn_callback(uint gpio, uint32_t events) {
-    if(events == 0x4){ //se o botao é precionado
-        if(gpio == BTN_PIN_G){
-            btn_g_press = true; //marca a flag
-        }
-        if(gpio == BTN_PIN_Y){
-            btn_y_press = true;
-        }
+    if(events == 0x4){ //botao apertado
+        btn_press = true;
+
+        //horario atual
+        t_inicial = to_ms_since_boot(get_absolute_time()); 
+    }
+    if(events == 0x8){ //botao solto
+        uint32_t t_final = to_ms_since_boot(get_absolute_time());
+
+        //horario final menos inicial
+        t_pressionado = t_final - t_inicial;
+        btn_press = false;
+        atualiza = true;
     }
 }
 
-bool timer_y_callback(repeating_timer_t *rt) {
-    pisca_y = true;
-    return true;
-
-}
-bool timer_g_callback(repeating_timer_t *rt) {
-    pisca_g = true;
+bool timer_callback(repeating_timer_t *rt) {
+     //so pisca se nao tiver pressionado e se esta ativo
+    if(!btn_press && ativo){
+        pisca = true;
+    }
     return true;
 }
 
-int64_t alarm_g_callback(alarm_id_t id, void *user_data) {
-    alarme_g = true;
-    return 0;
-
-}
-
-int64_t alarm_y_callback(alarm_id_t id, void *user_data) {
-    alarme_y = true;
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
+    parar = true;
     return 0;
 }
 
 int main() {
     stdio_init_all();
 
-    gpio_init(BTN_PIN_G);
-    gpio_set_dir(BTN_PIN_G, GPIO_IN);
-    gpio_pull_up(BTN_PIN_G);
-    gpio_set_irq_enabled_with_callback(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback);
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_put(LED_PIN, 0);
+    
+    gpio_init(BTN_PIN);
+    gpio_set_dir(BTN_PIN, GPIO_IN);
+    gpio_pull_up(BTN_PIN);
+    gpio_set_irq_enabled_with_callback(
+        BTN_PIN,
+        GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE,
+        true,
+        &btn_callback
+    );
 
-    gpio_init(BTN_PIN_Y);
-    gpio_set_dir(BTN_PIN_Y, GPIO_IN);
-    gpio_pull_up(BTN_PIN_Y);
-    gpio_set_irq_enabled_with_callback(BTN_PIN_Y, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback);
+    repeating_timer_t timer;
 
-    gpio_init(LED_PIN_G);
-    gpio_set_dir(LED_PIN_G, GPIO_OUT);
+    //timer pisca a cada 200ms
+    add_repeating_timer_ms(200, timer_callback, NULL, &timer);
 
-    gpio_init(LED_PIN_Y);
-    gpio_set_dir(LED_PIN_Y, GPIO_OUT);
+    bool led_estado = false;
 
-    //volatile alarm_id_t alarm_g;
-    //volatile alarm_id_t alarm_y;
+    while (true) {
 
-    repeating_timer_t time_g;
-    repeating_timer_t time_y;
-
-    bool led_estado_g = false;
-    bool led_estado_y = false;
-
-    bool rodando_g = false;
-    bool rodando_y = false;
-
-    while (1) {
-        if(btn_g_press && !rodando_g){ //precionou botao verde
-            btn_g_press = false;
-            rodando_g = true;
-            gpio_put(LED_PIN_G, 1);
-            add_alarm_in_ms(1000, alarm_g_callback, NULL, false);
-            add_repeating_timer_ms(200, timer_g_callback, NULL, &time_g);
+        // enquanto pressiona o LED apagado
+        if(btn_press){ 
+            gpio_put(LED_PIN,0);
         }
-        if(pisca_g){
-            pisca_g = false;
-            led_estado_g = !led_estado_g;
-            gpio_put(LED_PIN_G, led_estado_g);
-        } //para piscar
-        if(alarme_g){ //tempo acabou
-            alarme_g = false;
-            gpio_put(LED_PIN_G, 0);
-            cancel_repeating_timer(&time_g);
-            rodando_g = false;
-            if(rodando_y){ // para o amarelo tambem
-                rodando_y = false;
-                gpio_put(LED_PIN_Y,0);
-                cancel_repeating_timer(&time_y);
+
+        // piscar LED (só se ativo)
+        if(pisca && !btn_press && ativo){
+            led_estado = !led_estado;
+            gpio_put(LED_PIN, led_estado);
+            pisca = false;
+        }
+
+        // quando solta botão reinicia inicia ciclo
+        if(atualiza){
+            //garante q o tempo minimo seja 50 ms
+            if(t_pressionado < 50){
+                t_pressionado = 50;
             }
+
+            ativo = true; // ativa o sistema
+
+            //led pisca por t_pressionado de tempo
+            add_alarm_in_ms(t_pressionado, alarm_callback, NULL, false);
+
+            atualiza = false;
         }
 
-        if(btn_y_press && !rodando_y){
-            rodando_y = true;
-            btn_y_press = false;
-            gpio_put(LED_PIN_Y, 1);
-            add_alarm_in_ms(2000, alarm_y_callback, NULL, false);
-            add_repeating_timer_ms(500, timer_y_callback, NULL, &time_y);
+        // parar quando tempo acabar
+        if(parar){
+            parar = false;
+            ativo = false;
+            gpio_put(LED_PIN, 0);
         }
-        if(pisca_y){
-            pisca_y = false;
-            led_estado_y = !led_estado_y;
-            gpio_put(LED_PIN_Y, led_estado_y);
-        }
-        if(alarme_y){
-            alarme_y = false;
-            gpio_put(LED_PIN_Y, 0);
-            cancel_repeating_timer(&time_y);
-            rodando_y = false;
-            if(rodando_g){
-                rodando_g = false;
-                gpio_put(LED_PIN_G,0);
-                cancel_repeating_timer(&time_g);
-            }
-        }
-
     }
 
     return 0;
